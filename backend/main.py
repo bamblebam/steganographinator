6f68d9a8705ebabc086bf2c9fda5dffc9298260c
rishabh547 <rishabhkothari541@gmail.com>
2022-02-13 20:26:40 +0530

    Frontend Changes

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,39 @@
+from urllib import request
+import requests
 import streamlit as st
+from streamlit_lottie import st_lottie
 from PIL import Image
 import os
 from utils import *
 
+#Function to load the url
+def load_url(url:str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# load animation
+lottie_coding = load_url("https://assets6.lottiefiles.com/packages/lf20_cgjrfdzx.json")
 st.set_page_config(layout="wide")
-st.title("Steganography")
+
+
+
+col_1,col_2 = st.columns(2)
 col1,col2=st.columns(2)
 
 PATH=os.path.join(os.getcwd(),'assets\\enc_img.png')
+with col_1:
+    st.markdown("<h1> Steganographinator</h1", unsafe_allow_html=True)
+    st.write(">##### _'Welcome to your One Stop Solution for Steganography!'_")
+    st.write("* This is a tool to *hide your secret message* in an image. ")
+    st.write("* Keep your _Secrets to yourself_, and no more compromise with your security!")
+    st.write("* With _Caesar Cipher_ encyrpt images on the fly")
+with col_2:
+    # st.title("Steganography")
+    st_lottie(lottie_coding,width=400,height=300)
+   
+
 
 with col1:
     st.header("Encode Image")
@@ -33,3 +59,4 @@ with col2:
         dec_text=decode(img,key)
         st.write("Decoded Text: ")
         st.subheader(dec_text)
+        st.image(img,use_column_width=True)
